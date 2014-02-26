3a6d86c836ab74adc21be1428bebcaf3ca20b737
Len Kutchma <lkutchma@gmail.com>
2014-02-26 08:31:12 -0600

    added internationalization

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'malcolm', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/lib/languages', 'malcolm' ) );
+
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', __( 'Malcolm Theme', 'malcolm' ) );
 define( 'CHILD_THEME_URL', 'http://wpcanada.ca/our-themes/malcolm' );
